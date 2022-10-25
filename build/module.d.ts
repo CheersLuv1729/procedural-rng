7fc6c013290cb2221fde1081c7acf9533e33fd98
CheersLuv1729 <49064449+CheersLuv1729@users.noreply.github.com>
2022-10-25 22:34:21 +0100

    update gitignore

--- build/module.d.ts
+++ build/module.d.ts
@@ -1,7 +1,11 @@
+import Random, { RandomGenerator } from "./random";
 import MT19937 from "./generators/mt19937";
 import LCRNG, { LCRNG_Props } from "./generators/lcrng";
-import Random, { RandomGenerator } from "./random";
+import Xorshift32 from "./generators/xorshift32";
+import Xorshift128 from "./generators/xorshift128";
 export { MT19937 };
 export { LCRNG, LCRNG_Props };
+export { Xorshift32 };
+export { Xorshift128 };
 export { RandomGenerator };
-export default Random;
+export { Random };
--- build/module.js
+++ build/module.js
@@ -3,10 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-exports.LCRNG = exports.MT19937 = void 0;
+exports.Random = exports.Xorshift128 = exports.Xorshift32 = exports.LCRNG = exports.MT19937 = void 0;
+var random_1 = __importDefault(require("./random"));
+exports.Random = random_1["default"];
 var mt19937_1 = __importDefault(require("./generators/mt19937"));
 exports.MT19937 = mt19937_1["default"];
 var lcrng_1 = __importDefault(require("./generators/lcrng"));
 exports.LCRNG = lcrng_1["default"];
-var random_1 = __importDefault(require("./random"));
-exports["default"] = random_1["default"];
+var xorshift32_1 = __importDefault(require("./generators/xorshift32"));
+exports.Xorshift32 = xorshift32_1["default"];
+var xorshift128_1 = __importDefault(require("./generators/xorshift128"));
+exports.Xorshift128 = xorshift128_1["default"];
