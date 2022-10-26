9d9fdbf1e784acd2abe62f118d6f565f571ce3ae
CheersLuv1729 <49064449+CheersLuv1729@users.noreply.github.com>
2022-10-26 04:27:06 +0100

    Removed built files

--- build/generators/xorshift32.d.ts
+++
@@ -1,9 +0,0 @@
-import { RandomGenerator } from "../random";
-export default class Xorshift32 implements RandomGenerator {
-    private state;
-    constructor(seed?: number);
-    name(): string;
-    min(): number;
-    max(): number;
-    nextInt(): number;
-}
--- build/util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const uint: (i: number) => number;
-export declare const hash: (k: number) => number;
